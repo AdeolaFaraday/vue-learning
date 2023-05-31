0b7412de29b458c210f3b555d7bc6fa8468aeff5
AdeolaFaraday <adexconly@gmail.com>
2023-05-31 08:24:28 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TipContainer from "./components/TipContainer.vue";
+</script>
+
+<template>
+  <div class="container">
+    <TipContainer />
+  </div>
+</template>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@layer components {
+  /* h1 {
+    @apply font-bold underline;
+  } */
+  .container {
+    @apply flex justify-center items-center w-full h-screen;
+    background-color: #e6fffd;
+  }
+}
+</style>
---
+++ src/components/TipContainer.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@layer components {
+  /* h1 {
+    @apply font-bold underline;
+  } */
+  .container {
+    @apply h-3/5 w-3/5 rounded-2xl;
+    background-color: #fff;
+  }
+}
+</style>

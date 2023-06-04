8cb893867c647b12862e587691015b7a5bb891ca
AdeolaFaraday <adexconly@gmail.com>
2023-06-04 00:29:05 -0700

    add a fix height for mobile tip container

--- src/components/TipContainer.vue
+++ src/components/TipContainer.vue
@@ -27,11 +27,13 @@ import Text from "./Text/text.vue";
     @apply font-bold underline;
   } */
   .tipContainer {
-    @apply flex-col lg:flex-row lg:w-3/5 w-full lg:rounded-2xl rounded-t-3xl lg:p-10 p-6 flex justify-stretch gap-8;
+    @apply flex-col lg:flex-row lg:w-3/5 w-full lg:rounded-2xl rounded-t-3xl lg:p-10 p-6 flex justify-stretch gap-8 overflow-scroll lg:overflow-hidden;
     background-color: #fff;
     box-shadow: 25px 25px 50px 25px rgb(0 0 0 / 0.1);
+    height: 90vh;
     @media screen(lg) {
       box-shadow: 0px 25px 50px -12px rgb(0 0 0 / 0.2);
+      height: auto;
     }
   }
   .tipContainerItem {
